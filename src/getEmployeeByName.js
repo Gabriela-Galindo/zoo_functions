578f7981b096d86e144f16f4a8049f239e5bcfc6
Maria Gabriela Galindo <gabi_who@live.cm>
2022-09-29 17:38:46 -0300

    Adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    const noName = {};
+    return noName;
+  }
+  return data.employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
+
+// console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
