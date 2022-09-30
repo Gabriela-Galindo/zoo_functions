39c5524b0adf3ec10a21fccf9e46f2dd6e20582b
Maria Gabriela Galindo <gabi_who@live.cm>
2022-09-30 09:59:14 -0300

    Adiciona requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('deve retornar a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('deve retornar array com nomes dos elefantes', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('deve retornar média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5, 2);
+  });
+});
