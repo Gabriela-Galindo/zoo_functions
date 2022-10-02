fd0322f4e6fceaa963dddcc13d393848a4de3c58
Maria Gabriela Galindo <gabi_who@live.cm>
2022-10-02 09:42:47 -0300

    Adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  // let result = [];
+  return data.employees.filter((element) => element.managers.includes(managerId))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
