86dd00b6513557700fc3a6309e1ba6be7ebba852
Maria Gabriela Galindo <gabi_who@live.cm>
2022-10-03 16:12:10 -0300

    Adiciona requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const speciesManaged = data.employees.find((person) => person.id === id).responsibleFor[0];
+  const objectSpecies = data.species.find((animal) => animal.id === speciesManaged).residents
+    .sort((a, b) => b.age - a.age)[0];
+  return Object.values(objectSpecies);
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
