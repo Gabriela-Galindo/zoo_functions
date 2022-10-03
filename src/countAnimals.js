aa55d821c9176efb95505593c101124a5b24db67
Maria Gabriela Galindo <gabi_who@live.cm>
2022-10-03 11:51:54 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,13 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-//   if (animal === undefined) {
-//     const chave = data.species.map((species) => species.name);
-//     const valor = data.species.map((species) => species.residents.length);
-//     return
-//   }
+  if (animal === undefined) {
+    const newObject = {};
+    data.species.map((element) => {
+      newObject[element.name] = element.residents.length;
+      return true;
+    });
+    return newObject;
+  }
+  if ('sex' in animal) {
+    return data.species.find((target) => target.name === animal.specie)
+      .residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return data.species.find((target) => target.name === animal.specie).residents.length;
 }
 
-// console.log(countAnimals());
+// console.log(countAnimals({ specie: 'bears', sex: 'male' }));
 
 module.exports = countAnimals;
