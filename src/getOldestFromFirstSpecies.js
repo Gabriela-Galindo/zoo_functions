7aa27ce9490e18635adb332e30322aafe65ae204
Maria Gabriela Galindo <gabi_who@live.cm>
2022-10-03 16:12:48 -0300

    Corrige detalhe no requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -7,6 +7,6 @@ function getOldestFromFirstSpecies(id) {
   return Object.values(objectSpecies);
 }
 
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
