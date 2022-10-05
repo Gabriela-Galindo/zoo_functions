3279afa27ac221938347f7430d8086802603f714
Maria Gabriela Galindo <gabi_who@live.cm>
2022-10-05 10:31:06 -0300

    Adiciona algumas partes do requisito 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getSpecies(parametro) {
+  const animalName = data.species.find((animal) => animal.id === parametro).name;
+  const animalLocation = data.species.find((animal) => animal.id === parametro).location;
+  const resultAnimal = { species: [], locations: [] };
+  resultAnimal.species = animalName;
+  resultAnimal.locations = animalLocation;
+  return resultAnimal;
 }
+
+function getEmployeesCoverage(param) {
+  const resultPerson = { id: '', fullname: '' };
+  // eslint-disable-next-line array-callback-return
+  const personInfo = data.employees.find((person) => {
+    if (person.firstName === param.name
+      || person.lastName === param.name
+      || person.id === param.id) {
+      resultPerson.id = person.id;
+      resultPerson.fullname = `${person.firstName} ${person.lastName}`;
+      return resultPerson;
+    }
+  });
+  const animalsManaged = personInfo.responsibleFor.forEach(getSpecies());
+  return animalsManaged;
+}
+
+console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+// console.log(getSpecies('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const scheduleInfo = data.hours;
+const daysOfTheWeek = Object.keys(scheduleInfo);
+
+function getDay(day) {
+  if (day === 'Monday') {
+    return {
+      Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+    };
+  }
+  const animalExhibition = data.species.filter((specie) => specie.availability.includes(day))
+    .map((e) => e.name);
+  const { open, close } = scheduleInfo[day];
+  if (daysOfTheWeek.includes(day)) {
+    return {
+      [day]: {
+        officeHour: `Open from ${open}am until ${close}pm`,
+        exhibition: animalExhibition,
+      },
+    };
+  }
 }
 
+function getSchedule(scheduleTarget) {
+  if (data.species.map((element) => element.name).includes(scheduleTarget)) {
+    return data.species.find((animal) => animal.name === scheduleTarget).availability;
+  }
+  if (scheduleTarget === 'Monday') {
+    return getDay('Monday');
+  }
+  if (daysOfTheWeek.includes(scheduleTarget)) {
+    return getDay(scheduleTarget);
+  }
+  return getDay(daysOfTheWeek[0]);
+}
+
+console.log(getSchedule());
 module.exports = getSchedule;
