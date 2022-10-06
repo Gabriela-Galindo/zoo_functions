662da072e8a6b5671b3c7f714fe58a2fd6533721
Maria Gabriela Galindo <gabi_who@live.cm>
2022-10-06 13:49:58 -0300

    Corrige problemas com lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,39 +1,38 @@
-/* eslint-disable quote-props */
 const data = require('../data/zoo_data');
 
 const scheduleInfo = data.hours;
 const daysOfTheWeek = Object.keys(scheduleInfo);
 const objectResult = {
-  'Tuesday': {
-    'officeHour': 'Open from 8am until 6pm',
-    'exhibition': ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
   },
-  'Wednesday': {
-    'officeHour': 'Open from 8am until 6pm',
-    'exhibition': ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
   },
-  'Thursday': {
-    'officeHour': 'Open from 10am until 8pm',
+  Thursday: {
+    officeHour: 'Open from 10am until 8pm',
     exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
   },
-  'Friday': {
-    'officeHour': 'Open from 10am until 8pm',
-    'exhibition': ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
+  Friday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
   },
-  'Saturday': {
-    'officeHour': 'Open from 8am until 10pm',
-    'exhibition': [
+  Saturday: {
+    officeHour: 'Open from 8am until 10pm',
+    exhibition: [
       'lions', 'tigers',
       'bears', 'penguins',
       'otters', 'frogs',
       'snakes', 'elephants',
     ],
   },
-  'Sunday': {
-    'officeHour': 'Open from 8am until 8pm',
-    'exhibition': ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
+  Sunday: {
+    officeHour: 'Open from 8am until 8pm',
+    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
   },
-  'Monday': { 'officeHour': 'CLOSED', exhibition: 'The zoo will be closed!' },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
 };
 
 function getDay(day) {
