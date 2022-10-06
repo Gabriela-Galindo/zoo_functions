31003bc71f6c84d0f30848e84bc23213e5395563
Maria Gabriela Galindo <gabi_who@live.cm>
2022-10-06 13:42:49 -0300

    Adiciona requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,40 @@
+/* eslint-disable quote-props */
 const data = require('../data/zoo_data');
 
 const scheduleInfo = data.hours;
 const daysOfTheWeek = Object.keys(scheduleInfo);
+const objectResult = {
+  'Tuesday': {
+    'officeHour': 'Open from 8am until 6pm',
+    'exhibition': ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  },
+  'Wednesday': {
+    'officeHour': 'Open from 8am until 6pm',
+    'exhibition': ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  },
+  'Thursday': {
+    'officeHour': 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
+  },
+  'Friday': {
+    'officeHour': 'Open from 10am until 8pm',
+    'exhibition': ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
+  },
+  'Saturday': {
+    'officeHour': 'Open from 8am until 10pm',
+    'exhibition': [
+      'lions', 'tigers',
+      'bears', 'penguins',
+      'otters', 'frogs',
+      'snakes', 'elephants',
+    ],
+  },
+  'Sunday': {
+    'officeHour': 'Open from 8am until 8pm',
+    'exhibition': ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
+  },
+  'Monday': { 'officeHour': 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
 
 function getDay(day) {
   if (day === 'Monday') {
@@ -22,8 +55,6 @@ function getDay(day) {
   }
 }
 
-const objectResult = getDay(daysOfTheWeek[0]);
-
 function getSchedule(scheduleTarget) {
   if (data.species.map((element) => element.name).includes(scheduleTarget)) {
     return data.species.find((animal) => animal.name === scheduleTarget).availability;
@@ -34,8 +65,8 @@ function getSchedule(scheduleTarget) {
   if (daysOfTheWeek.includes(scheduleTarget)) {
     return getDay(scheduleTarget);
   }
-  return getDay(daysOfTheWeek[0]);
+
+  return objectResult;
 }
 
-console.log(objectResult);
 module.exports = getSchedule;
